46304e0b090a70efdc5ec4a7f93724ba71d4345b
Maksim Gurkov <maksim.gurkov@gmail.com>
2022-05-17 15:19:27 +0300

    Initial Commit

---
+++ AnimationSwiftUi/AnimationSwiftUiApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationSwiftUiApp.swift
+//  AnimationSwiftUi
+//
+//  Created by Максим Гурков on 17.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct AnimationSwiftUiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
